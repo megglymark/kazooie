04ef04c5a66d2767bd5c10d53ce9034cb9803078
Mark Moniz <megglymark@gmail.com>
2016-02-22 19:17:10 -0800

    Adding user routes to app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/user', users);
 
 // passport config
 var Account = require('./models/account');
